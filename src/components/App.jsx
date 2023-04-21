196c5cf8fa006189146556fffb3c5c0878cfd53f
Muhammad Irfan Malik <irfan.rala@gmail.com>
2023-04-21 14:12:47 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>User Login </h1>
+      <br />
+      <input type="text" placeholder="User name" />
+      <input type="password" placeholder="password" />
+      <button>login</button>
+    </div>
+  );
+}
+
+export default App;
